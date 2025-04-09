a90ade0f65b54a1b60ac9ac0d57a53617f30e8e0
Yael Velazquez <yayo.velazquez@outlook.com>
2025-04-09 17:11:31 -0600

    Versión 2: Ver tareas

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ with open("tareas.txt", "a") as archivo:
     tarea = input("Escribe una nueva tarea: ")
     archivo.write(tarea + "\n")
     print("Tarea guardada.")
-    
+
+    print("\nTus tareas:")
+with open("tareas.txt", "r") as archivo:
+    print(archivo.read())
+
 
 
