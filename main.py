2793e4b898bdb6e5d527a11038a37a04890152cd
Yael Velazquez <yayo.velazquez@outlook.com>
2025-04-09 17:12:11 -0600

    Versión con menú

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-print("¡Bienvenido al gestor de tareas!")
+def agregar_tarea():
+    with open("tareas.txt", "a") as archivo:
+        tarea = input("Escribe una nueva tarea: ")
+        archivo.write(tarea + "\n")
+        print("Tarea guardada.")
 
-with open("tareas.txt", "a") as archivo:
-    tarea = input("Escribe una nueva tarea: ")
-    archivo.write(tarea + "\n")
-    print("Tarea guardada.")
-
+def ver_tareas():
     print("\nTus tareas:")
-with open("tareas.txt", "r") as archivo:
-    print(archivo.read())
+    with open("tareas.txt", "r") as archivo:
+        print(archivo.read())
+
+while True:
+    print("\nGestor de Tareas")
+    print("1. Agregar tarea")
+    print("2. Ver tareas")
+    print("3. Salir")
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        agregar_tarea()
+    elif opcion == "2":
+        ver_tareas()
+    elif opcion == "3":
+        break
+    else:
+        print("Opción no válida")
 
 
 
